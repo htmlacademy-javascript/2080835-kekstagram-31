79c0461d135c55a64aa8eb92b0e7f7ce28f82cad
Kiselev Egor <kiselevegor1996@mail.ru>
2024-03-05 20:16:45 +0300

    Функция возвращается

--- js/function.js
+++ js/function.js
@@ -30,4 +30,20 @@ isPalindrome('Кекс'); // false
 // Это палиндром
 isPalindrome('Лёша на полке клопа нашёл '); // true
 
+/*
+'8:00' - начало рабочего дня
+'17:30' - конец рабочего дня
+'14:00' - начало встречи
+90 - продолжительность встречи в минутах
+*/
+// eslint-disable-next-line no-console, no-undef
+console.log(checkMeetingTime('08:00', '17:30', '14:00', 90)); // true
+// eslint-disable-next-line no-console, no-undef
+console.log(checkMeetingTime('8:0', '10:0', '8:0', 120)); //true
+// eslint-disable-next-line no-console, no-undef
+console.log(checkMeetingTime('08:00', '14:30', '14:00', 90)); //false
+// eslint-disable-next-line no-console, no-undef
+console.log(checkMeetingTime('14:00', '17:30', '08:0', 90)); //false
+// eslint-disable-next-line no-console, no-undef
+console.log(checkMeetingTime('8:00', '17:30', '08:00', 900)); //false
 
